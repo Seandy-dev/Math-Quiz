71d643e92912f7f90c215868fcaf3834d82c1398
Sindiswa Mbhele <75256216+Seandy-dev@users.noreply.github.com>
2021-06-03 13:21:23 +0200

    Added three files

---
+++ division.js
@@ -0,0 +1,25 @@
+function check(){
+	var c=0;
+	var q1=document.division.question1.value;
+	var q2=document.division.question2.value;
+	var q3=document.division.question3.value;
+	var q4=document.division.question4.value;
+	var result= document.getElementById('result');
+	//var division= document.getElementById('division');
+	
+	if(q1=="32"){c++}
+	if(q2=="37"){c++}
+	if(q3=="90"){c++}
+	if(q4=="22"){c++}
+	
+	//division.style.display="none";
+	
+	//if(c<=3){
+	//result.textContent='Your marks is ${c} It is not good so please work on yourself.';
+	//}else{
+	//	result.textContent='Keet it up.';
+	//}
+	
+	//result.textContent='${c)';
+	
+}
